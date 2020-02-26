b985b01faa8ac430fe45acecddc200284a9b2aed
Aleksiel <aleksielhorak@gmail.com>
2020-02-26 10:19:59 +0200

    Vid 17: Component options

--- plugins/ahorak/movies/components/Actors.php
+++ plugins/ahorak/movies/components/Actors.php
@@ -22,7 +22,22 @@ class Actors extends ComponentBase {
                 'default' => 0, // default value of option. (0 = displays all)
                 'validationPattern' => '^[0-9]+$', // regex validation pattern for numbers
                 'validatonMessage' => 'Enter only numbers' // define validation messahe
+            ],
+
+            'sortOrder' => [
+                'title' => 'Sort Actors',
+                'description' => 'Sort Actors list',
+                'type' => 'dropdown', // type of option
+                'default' => 'name asc'
             ]
+        ];
+    }
+
+    public function getSortOrderOptions() { //define and get the options for the sortOrder
+        
+        return [
+            'name asc' => 'Name (ascending)',    // left = "machine name", right = what user will see
+            'name desc' => 'Name (descending)'
         ];
     }
 
@@ -32,9 +47,19 @@ class Actors extends ComponentBase {
 
     protected function loadActors() {          // Setting the data for the component
         
-        $query = Actor::all(); 
+        $query = Actor::all(); // display all (aka if default 0 for 'results' is entered)
 
-        if($this->property('results') > 0) { // Checking if the property named results if greater than 0
+        if($this->property('sortOrder') == 'name asc') { // checking if 'sortOrder' is name asc
+
+            $query = $query->sortBy('name'); // Laravel method to sort ascending (case sensitive)
+        }
+
+        if($this->property('sortOrder') == 'name desc') { // checking if 'sortOrder' is name desc
+
+            $query = $query->sortByDesc('name'); // Laravel method to sort descending (case sensitive)
+        }
+
+        if($this->property('results') > 0) { // Checking if the value inputted in property 'results' is greater than 0
 
             $query = $query->take($this->property('results')); // taking the amount given by property results
         }
