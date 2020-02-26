d9dd4a9dbdfee04c3213afd662d42a74b5454408
Aleksiel <aleksielhorak@gmail.com>
2020-02-26 15:17:53 +0200

    Vid 19: Form Validation

--- plugins/ahorak/contact/components/ContactForm.php
+++ plugins/ahorak/contact/components/ContactForm.php
@@ -6,6 +6,10 @@ use Input; // Needed to get input values
 
 use Mail; // Needed to be able to send emails
 
+use Validator; // Needed to validate the form
+
+use Redirect; // Needed to redirect page after validation
+
 class ContactForm extends ComponentBase {
 
     // Define component details
@@ -19,16 +23,37 @@ class ContactForm extends ComponentBase {
 
     public function onSend() { // This will be bound to our contact form
 
-        $vars = ['name' => Input::get('email'), 
+        $validator = Validator::make(
+            [
+                'name' => Input::get('name'),
+                'email' => Input::get('email')
+            ],
+            [
+                'name' => 'required',
+                'email' => 'required|email'
+            ]
+        );
+        
+        if($validator->fails()) {
+            // Don't send email. Send error
+
+            return Redirect::back()->withErrors($validator); // Return to the page you're currently on, with errors
+            
+        } else {
+            // Send email
+
+            $vars = ['name' => Input::get('name'), 
                 'email' => Input::get('email'),
                 'content' => Input::get('content')
                 ];
             // Input::get('var') is used to get the value from the form
 
-        Mail::send('ahorak.contact::mail.message', $vars, function($message) {
-            $message->to('admin@domain.tld', 'Admin Person');
-            $message->subject('New message from contact form');
-        });
+            Mail::send('ahorak.contact::mail.message', $vars, function($message) {
+                $message->to('admin@domain.tld', 'Admin Person');
+                $message->subject('New message from contact form');
+            });
+        }
+        
     }
 
 }
